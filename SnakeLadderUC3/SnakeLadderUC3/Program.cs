0370a839901fb12422d26067c4f909e638227156
naveen962 <mrn28897@gmail.com>
2021-09-24 04:40:48 -0700

    Snake and Ladder using switch statement  program is added

---
+++ SnakeLadderUC3/SnakeLadderUC3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SnakeLadderUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerpos = 0;
+            
+            Random ran=new Random();
+            int rolldice=ran.Next(1, 7);
+            Console.WriteLine("Player gets = " + rolldice);
+            int choice = ran.Next(0, 3);
+            Console.WriteLine("Choice = "+choice);
+            const int NoPlay = 0;
+            const int Ladder = 1;
+            const int Snake = 2;
+            switch (choice)
+            {
+                case NoPlay:
+                    break;
+
+                case Ladder:
+                    playerpos = playerpos + rolldice;
+                    Console.WriteLine("Player at position = " +playerpos);
+                    break;
+                case Snake:
+                    playerpos = playerpos - rolldice;
+                    Console.WriteLine("Player at position = " +playerpos);
+                    break;
+
+            }
+        }
+    }
+}
