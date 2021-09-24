feb40fa8a6f6e349e2de21cf6b28fac6fca2f9a3
naveen962 <mrn28897@gmail.com>
2021-09-24 04:31:20 -0700

    calculating the player rolls the die to get random number program is added

---
+++ SnakeLadderUC2/SnakeLadderUC2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeLadderUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerpos = 0;
+            Random ran = new Random();
+            int rolldicePlayer1=ran.Next(0, 7);
+            Console.WriteLine(" Player1 dice rolled and get = " + rolldicePlayer1);
+           
+        }
+    }
+}
