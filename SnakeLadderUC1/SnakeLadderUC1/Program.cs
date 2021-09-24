84b9850fccf3125d7266bb422c82699f663ea42d
naveen962 <mrn28897@gmail.com>
2021-09-24 04:26:56 -0700

    Snake and Ladder with single player at start position 0 program is added

---
+++ SnakeLadderUC1/SnakeLadderUC1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadderUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int  PlayerPos = 0;
+            Console.WriteLine("Player at Position = " +PlayerPos);
+        }
+    }
+}
